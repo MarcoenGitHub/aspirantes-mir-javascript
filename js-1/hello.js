fb0ef2620d135a927875347aac8f2c3d5dd4b71c
Marco <marcokfernandez@gmail.com>
2023-06-14 21:11:07 -0500

    solucion a los ejercicios de js

---
+++ js-1/hello.js
@@ -0,0 +1,10 @@
+// escribe la función hola acá
+function hola(nombre){
+    return "Hola " + nombre + "!"
+//o return `hola ${nombre}!`
+}
+
+// código de prueba
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("Marco")) // "Hola !"
---
+++ js-1/sum.js
@@ -0,0 +1,13 @@
+// escribe la función suma acá
+function sum(numero){
+    return (numero * (numero + 1)) / 2
+    // ó let total = 0
+    //   for(let i = 1; i ≤ numero ; i++) {
+    //     total = i + total
+    }
+
+
+// código de prueba
+console.log(sum(4)) // 1 + 2 + 3 + 4 = 10
+console.log(sum(10)) // 55
+console.log(sum(15)) // 120
